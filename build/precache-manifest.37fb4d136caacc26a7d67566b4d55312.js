ce2c363c7d60be6d999eeb210c6341b0e9192543
muzzammiljamil22 <muzzammil2242@gmail.com>
2020-09-07 13:08:58 +0500

    commit for front-end changes for the deployment to AWS dev

---
+++ build/precache-manifest.37fb4d136caacc26a7d67566b4d55312.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc797efbb4f55de51c718b500000b969",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90e4ddfbadd9aee073ed",
+    "url": "./static/css/2.5503be41.chunk.css"
+  },
+  {
+    "revision": "8421db4440b5eea71f48",
+    "url": "./static/css/main.ce621076.chunk.css"
+  },
+  {
+    "revision": "90e4ddfbadd9aee073ed",
+    "url": "./static/js/2.d64189c0.chunk.js"
+  },
+  {
+    "revision": "8421db4440b5eea71f48",
+    "url": "./static/js/main.47dec4ba.chunk.js"
+  },
+  {
+    "revision": "630078f4209429a71e9b",
+    "url": "./static/js/runtime~main.de0ec639.js"
+  },
+  {
+    "revision": "8d66b7150ec5409a8ac2bfedbae1e2f0",
+    "url": "./static/media/KHMC Circle Logo.8d66b715.png"
+  },
+  {
+    "revision": "629cedf6e76b525f35115ab3d40c7242",
+    "url": "./static/media/Splash.629cedf6.png"
+  },
+  {
+    "revision": "4dff5aea3215cb8df168fac73cd7a408",
+    "url": "./static/media/ThankYou.4dff5aea.png"
+  },
+  {
+    "revision": "709ac24547ea8f154b691ebb2156447b",
+    "url": "./static/media/WMS_Back.709ac245.png"
+  },
+  {
+    "revision": "f73a030762eb66fb6b0cb6d6a59b3a72",
+    "url": "./static/media/print.f73a0307.png"
+  }
+]);
