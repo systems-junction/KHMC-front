a8791dfc00f48b61a6d523e3b624062279bbf830
muzzammiljamil22 <muzzammil2242@gmail.com>
2020-10-08 20:48:54 +0500

    commit for front-end changes made for the deployment to the DO dev server

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c483c240ac767a3c27b19852f72f0190.js"
+  "./precache-manifest.ddf5f12d3c36c492b62e7ac429cb263a.js"
 );
 
 self.addEventListener('message', (event) => {
