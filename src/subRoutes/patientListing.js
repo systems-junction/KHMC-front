ec5cc5be3f7f39613f58da6a022c7d5beb5d61b4
Izaz Khan <izazkhan2000@gmail.com>
2020-10-14 16:48:28 +0500

    Patient registration refactoring and bug fixed

--- src/subRoutes/patientListing.js
+++ src/subRoutes/patientListing.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import NotFound from '../components/NotFound/NotFound'
 import SuccessScreen from '../components/SuccessScreen/SuccessScreen'
-import AddEditPatientListing from '../views/PatientListing/addEditPatientListing'
+// import AddEditPatientListing from '../views/PatientListing/addEditPatientListing'
+import AddEditPatientListing from '../components/PatientRegistration/PatientRegistration'
 import patientListing from '../views/PatientListing/patientListing'
 
 class PatientListing extends React.PureComponent {
