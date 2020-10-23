85cd41400a68833fcd42ba8e1032fa655a72226a
Saad <pmdevteam0@gmail.com>
2020-10-23 14:15:16 +0500

    build edit

---
+++ build/precache-manifest.919034e1b6f7d72ea300d2f4883f303b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff0709ba7475b69a8a18241e7aa85eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c30f4d3cb60012d5bf2e",
+    "url": "./static/css/2.73342d6f.chunk.css"
+  },
+  {
+    "revision": "0e60b90bdd7b0c00ee9f",
+    "url": "./static/css/main.8fb7821e.chunk.css"
+  },
+  {
+    "revision": "c30f4d3cb60012d5bf2e",
+    "url": "./static/js/2.461a8e3c.chunk.js"
+  },
+  {
+    "revision": "2cd65ab675b62c21b6cc",
+    "url": "./static/js/3.8f490ddf.chunk.js"
+  },
+  {
+    "revision": "d75089ad44ceccc93857",
+    "url": "./static/js/4.2f9de250.chunk.js"
+  },
+  {
+    "revision": "9eaa5cfab4890e18f834",
+    "url": "./static/js/5.6e2b5f50.chunk.js"
+  },
+  {
+    "revision": "0e60b90bdd7b0c00ee9f",
+    "url": "./static/js/main.1d0207c4.chunk.js"
+  },
+  {
+    "revision": "c6c2c0a40dc0cd09766b",
+    "url": "./static/js/runtime~main.517c5437.js"
+  },
+  {
+    "revision": "8a31207c3c400a14c836501add081c80",
+    "url": "./static/media/25.8a31207c.png"
+  },
+  {
+    "revision": "dc507e31c5f470244030763d33ef6e46",
+    "url": "./static/media/Influence Original.dc507e31.png"
+  },
+  {
+    "revision": "e88f171d96d8ac4b5ef70041e8a28857",
+    "url": "./static/media/KHMC Header LOGO.e88f171d.png"
+  },
+  {
+    "revision": "629cedf6e76b525f35115ab3d40c7242",
+    "url": "./static/media/Splash.629cedf6.png"
+  },
+  {
+    "revision": "4dff5aea3215cb8df168fac73cd7a408",
+    "url": "./static/media/ThankYou.4dff5aea.png"
+  },
+  {
+    "revision": "709ac24547ea8f154b691ebb2156447b",
+    "url": "./static/media/WMS_Back.709ac245.png"
+  },
+  {
+    "revision": "c23c6a6040214476d211f708fdf1fc9c",
+    "url": "./static/media/khmc_circle_logo.c23c6a60.png"
+  },
+  {
+    "revision": "ffd04bc24b9faaad2fe074738b9f5941",
+    "url": "./static/media/logoPatientSummaryInvoice.ffd04bc2.png"
+  },
+  {
+    "revision": "f73a030762eb66fb6b0cb6d6a59b3a72",
+    "url": "./static/media/print.f73a0307.png"
+  }
+]);
