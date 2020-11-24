c7cf12ca218f69309a60a47ac361af5842e264a8
Abdul Hannan Butt <hannanbutt1995@gmail.com>
2020-11-24 19:05:19 +0500

    buid

---
+++ build/precache-manifest.0d3dba8646690553df2c54bdd4e52878.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eee26a0918855459557fe1c6bc7ecd3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9391a60bbd2a0052f563",
+    "url": "./static/css/2.f80b2b94.chunk.css"
+  },
+  {
+    "revision": "ecaa54a9ccf56f4e9a9e",
+    "url": "./static/css/main.d276e6a7.chunk.css"
+  },
+  {
+    "revision": "9391a60bbd2a0052f563",
+    "url": "./static/js/2.68b3088b.chunk.js"
+  },
+  {
+    "revision": "aef21235e9e05c2c2fc8",
+    "url": "./static/js/3.d5f77262.chunk.js"
+  },
+  {
+    "revision": "8b0e54786ce858dc8f8a",
+    "url": "./static/js/4.057ffbb5.chunk.js"
+  },
+  {
+    "revision": "e4bbe3694b639bf553cc",
+    "url": "./static/js/5.e68def0b.chunk.js"
+  },
+  {
+    "revision": "ecaa54a9ccf56f4e9a9e",
+    "url": "./static/js/main.2c463593.chunk.js"
+  },
+  {
+    "revision": "eb381ca529fd4144e96f",
+    "url": "./static/js/runtime~main.2a074037.js"
+  },
+  {
+    "revision": "8a31207c3c400a14c836501add081c80",
+    "url": "./static/media/25.8a31207c.png"
+  },
+  {
+    "revision": "7c659c8466e55568683b89e94be983f0",
+    "url": "./static/media/Influence Original - White.7c659c84.png"
+  },
+  {
+    "revision": "dc507e31c5f470244030763d33ef6e46",
+    "url": "./static/media/Influence Original.dc507e31.png"
+  },
+  {
+    "revision": "68ec89a9f236eab47fcfc4acdc2ba97a",
+    "url": "./static/media/KHMC Header LOGO.68ec89a9.png"
+  },
+  {
+    "revision": "629cedf6e76b525f35115ab3d40c7242",
+    "url": "./static/media/Splash.629cedf6.png"
+  },
+  {
+    "revision": "4dff5aea3215cb8df168fac73cd7a408",
+    "url": "./static/media/ThankYou.4dff5aea.png"
+  },
+  {
+    "revision": "709ac24547ea8f154b691ebb2156447b",
+    "url": "./static/media/WMS_Back.709ac245.png"
+  },
+  {
+    "revision": "843c03ed6edd3f2a4dc35357feedbbec",
+    "url": "./static/media/icon-a.843c03ed.svg"
+  },
+  {
+    "revision": "d70663d06bd44437c178f522047470cc",
+    "url": "./static/media/icon-b.d70663d0.svg"
+  },
+  {
+    "revision": "6f440c0d2fa852de523edfd5383ad682",
+    "url": "./static/media/icon-c.6f440c0d.svg"
+  },
+  {
+    "revision": "4ef3abbd14fbe85ceebf36d900d2c9ad",
+    "url": "./static/media/icon-d.4ef3abbd.svg"
+  },
+  {
+    "revision": "c23c6a6040214476d211f708fdf1fc9c",
+    "url": "./static/media/khmc_circle_logo.c23c6a60.png"
+  },
+  {
+    "revision": "ffd04bc24b9faaad2fe074738b9f5941",
+    "url": "./static/media/logoPatientSummaryInvoice.ffd04bc2.png"
+  },
+  {
+    "revision": "f73a030762eb66fb6b0cb6d6a59b3a72",
+    "url": "./static/media/print.f73a0307.png"
+  }
+]);
