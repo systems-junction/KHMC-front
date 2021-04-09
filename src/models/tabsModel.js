b8c274637b717d197e48b507db1357647cf26a76
Muhammad Usman <usmangq12@gmail.com>
2021-04-09 20:26:51 +0500

    Worked upon WMS to display dynamic inner tabs

--- src/models/tabsModel.js
+++ src/models/tabsModel.js
@@ -1,6 +1,16 @@
-const generateTabsModel = (id, defaultValue, tabs) => ({
+const generateTabsModel = (
+  id,
+  defaultValue,
+  innerDefaultValue,
+  tabs,
+  innerTabs,
+  mainTabSelected
+) => ({
   id: id,
   value: defaultValue,
+  innerValue: innerDefaultValue,
   tabs: tabs,
+  innerTabs: innerTabs,
+  mainTabSelected: mainTabSelected,
 });
 export default generateTabsModel;
